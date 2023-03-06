4a2e1a7e7004174fa97bdf1d3f2e761f1a9b6ae8
Rishi <rishishahi92@gmail.com>
2023-03-06 12:38:38 +0530

    added new sections in home page

--- home-index.js
+++ home-index.js
@@ -2,6 +2,11 @@ const districtMenu = document.querySelector(".district-menu");
 const selectMenuBtn = document.querySelector(".select-menu-btn");
 const selectText = document.querySelector(".select-text");
 const districtOptions = document.querySelectorAll(".district-option");
+const kannadaMainNav = document.querySelector(".kannada-main-nav");
+const kannadaSubNav = document.querySelector(".kannada-sub-nav");
+const header = document.querySelector("header");
+const kannadaBreakingNews = document.querySelector(".kannada-breaking-news");
+const kannadaTheme = document.querySelector(".kannada-theme");
 
 const icon = document.querySelector(".nav-icon");
 const megaMenu = document.querySelector(".kannada-mega-menu");
@@ -15,6 +20,19 @@ const arrowIcons = document.querySelectorAll(".arrow-icon i");
 
 const trending = document.querySelector(".trending-topics");
 const trendingArrowIcons = document.querySelectorAll(".trending-arrow-icon i");
+
+const headerBottom = header.getBoundingClientRect().bottom;
+const kannadaNavHeight = kannadaMainNav.getBoundingClientRect().bottom;
+
+window.addEventListener("load", function () {
+  kannadaMainNav.style.top = `${headerBottom}px`;
+  kannadaTheme.style.marginTop = `${headerBottom + kannadaNavHeight}px`;
+});
+
+window.addEventListener("resize", function () {
+  kannadaMainNav.style.top = `${headerBottom}px`;
+  kannadaTheme.style.marginTop = `${headerBottom + kannadaNavHeight}px`;
+});
 
 selectMenuBtn.addEventListener("click", function () {
   districtMenu.classList.toggle("active");
@@ -57,5 +75,16 @@ arrowIcons.forEach(function (icon) {
 trendingArrowIcons.forEach(function (icon) {
   icon.addEventListener("click", function (e) {
     trending.scrollLeft += e.target.id === "left" ? -100 : 100;
+  });
+});
+
+// details page js
+
+const arrowIcon = document.querySelectorAll(".arrow-icon");
+const relatedStories = document.querySelector(".related-stories-card");
+
+arrowIcon.forEach(function (icon) {
+  icon.addEventListener("click", function (e) {
+    relatedStories.scrollLeft += e.target.id === "left" ? -300 : 300;
   });
 });
